d3a43aa1b76c0c7ff49ead83e1b0f092c907dfb9
MarkoDan <dankovic.marko@yahoo.com>
2022-09-11 21:57:07 +0200

    final form

--- scrape.py
+++ scrape.py
@@ -1,20 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 response = requests.get('https://news.ycombinator.com/news')
+response2 = requests.get('https://news.ycombinator.com/news?p=2')
 soupObject = BeautifulSoup(response.text, 'html.parser')
-#print(soupObject.head)
-# print(soupObject.find_all('div'))
-# print(soupObject.title)
-# print(soupObject.a)
-# print(soupObject.find(id="score_32787251"))
+soupObject2 = BeautifulSoup(response2.text, 'html.parser')
 
-# print(soupObject.select('.score'))
+links = soupObject.select('.titlelink')
+subtex = soupObject.select('.subtext')
+links2 = soupObject2.select('.titlelink')
+subtex2 = soupObject2.select('.subtext')
 
-# print(soupObject.select('#score_32780388'))
+mega_links = links + links2
+mega_subtext = subtex + subtex2
 
-link = soupObject.select('.titlelink')
-votes = soupObject.select('.score')
-print(votes[2])
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key= lambda k:k['votes'], reverse=True)
 
-print(votes[0].get('id'))
+
+def create_custom_hk(links, subtext):
+    hn = []
+    for index, item in enumerate(links):
+        title = links[index].getText()
+        href = links[index].get('href', None)
+        vote = subtext[index].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace('points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return sort_stories_by_votes(hn)
+     
+
+pprint.pprint(create_custom_hk(mega_links, mega_subtext))
